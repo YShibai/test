b2b01471f6d4c4027a8f3a67a3df3e397c041edc
c0113248 <c0113248>
2016-07-21 16:57:08 +0900

    同様に変更

--- tree.js
+++ tree.js
@@ -11,12 +11,12 @@ fs.readdir(_dir, function(err, files){
 
     // filesの中身を繰り替えして出力
     for(var h=0;h<files.length; h++){
-        if(fs.statSync(_dir + "\\" + file).isFile()){
+        if(fs.statSync(_dir + "\\" + files[h]).isFile()){
         }else{
-            arei[i]=_dir + "\\" + file;
+            arei[i]=_dir + "\\" + files[h];
             var tmp=arei[i];
             i+=1;
-            tv += "<li><a href='javascript:void(0)' onClick='javascript:void(0)'>" +file + "</a><ul>";
+            tv += "<li><a href='javascript:void(0)' onClick='javascript:void(0)'>" +files[h] + "</a><ul>";
                     fs.readdir(tmp, function(err, files2){
                       for(var j=0; j<files2.length; j++){
                           if(fs.statSync(tmp + "\\" + files2[j]).isFile()){
